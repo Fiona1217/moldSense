6954f477863905c00aa33d393d63ad63a55292f7
Fiona Ang <fionaang2006@yahoo.com>
2025-09-07 04:47:20 -0400

    ist to dist

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,8 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   build: {
-    outDir: 'ist'
+    outDir: 'dist',
+    emptyOutDir: true
   },
   
   // Server configuration
